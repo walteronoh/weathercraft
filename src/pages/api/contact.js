e34a81347910479a5d8fc58d81133ccd656fb811
Walter Kiprono <walterkiprono81@gmail.com>
2023-06-01 22:01:49 +0300

    chore: re-configure cors

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -4,17 +4,32 @@ import { sendMail } from "../../utils/mail"
 
 const cors = Cors({ origin: "https://www.walterkiprono.com" });
 
-export default function handler(req, res) {
-  // Run the CORS middleware
-  cors(req, res, async () => {
-    try {
-      sendMail(req.body).then((resp) => {
-        res.status(200).json({ message: "Message Sent" });
-      }).catch((err) => {
-        res.status(400).json(err);
-      })
-    } catch (e) {
-      res.status(400).json(e);
-    }
+function runMiddleWare(req, res, middleWare) {
+  return new Promise((resolve, reject) => {
+    middleWare(req, res, (result) => {
+      if (result instanceof Error) {
+        reject(result);
+      } else {
+        resolve(result);
+      }
+    });
   });
+};
+
+export default async function handler(req, res) {
+  // Handle the preflight request
+  if (req.method === "OPTIONS") {
+    await runMiddleWare(req, res, cors);
+    return;
+  }
+  try {
+    await runMiddleWare(req, res, cors);
+    sendMail(req.body).then((resp) => {
+      res.status(200).json({ message: "Message Sent" });
+    }).catch((err) => {
+      res.status(400).json(err);
+    })
+  } catch (e) {
+    res.status(400).json(e);
+  }
 }
