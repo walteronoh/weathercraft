d700b841f2b32479a81109f1cec5a07b97b15777
Walter Kiprono <walterkiprono81@gmail.com>
2023-10-01 15:43:17 +0300

    chore: change app name to WeatherCraft

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,11 +4,11 @@ import styles from "./footer.module.css";
 export default function Footer(props) {
     return (
         <footer className={styles.footer}>
-            <ul>
+            {/* <ul>
                 <li><a target="_blank" rel="noreferrer" href='https://twitter.com/_wkiprono'><Image src="/images/twitter.png" height={20} width={20} alt="Twitter icon" /></a></li>
                 <li><a target="_blank" rel="noreferrer" href='https://www.linkedin.com/in/walter-kiprono-4212381a0/'><Image src="/images/linkedin.png" height={20} width={20} alt="LinkedIn icon" /></a></li>
                 <li><a target="_blank" rel="noreferrer" href='https://github.com/walteronoh'><Image src="/images/github-light.png" height={20} width={20} alt="Github icon" /></a></li>
-            </ul>
+            </ul> */}
             {props.accredit}
         </footer>
     )
